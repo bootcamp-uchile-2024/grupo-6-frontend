be0880289daf2dcd145e42051fde399ad59fb81a
Daniela Gajardo <dgajardoiglesias@gmail.com>
2024-08-24 10:28:52 -0400

    Se arreglan img del Footer

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,11 +1,11 @@
-import icono_imagen from './assets/images/icono_imagen.png'
+import icono_mercadopago from './assets/images/logo-mercadopago29.png'
+import icono_paypal from './assets/images/Paypal_2014_logo.png'
+import icono_webpay from './assets/images/logo-webpay-plus-3-2.png'
 import './styles/estilos_home.css'
 
-function FooterFin() {
+function Footer() {
 
   return (
-    <>
-        <div className="footer"></div>
         <footer id="Footer">
 
             <div className="cajaFooter paginasSelectas">
@@ -31,9 +31,9 @@ function FooterFin() {
             <div className="cajaFooter formasPago">
                 <p className="nombresFooter">Formas de pago</p>
                 <div className="imagenesPago">
-                    <img src="images/logo-mercadopago29.png" alt="mercadoPago">
-                    <img src="images/Paypal_2014_logo.png" alt="payPal">
-                    <img src="images/logo-webpay-plus-3-2.png" alt="webPay">
+                    <img src={icono_mercadopago} alt="mercadoPago"/>
+                    <img src={icono_paypal} alt="payPal"/>
+                    <img src={icono_webpay} alt="webPay"/>
                 </div>
             </div>
 
@@ -50,10 +50,8 @@ function FooterFin() {
             <div className="copyright">
                 <p>Copyright © 2024 HexaStudio.</p>
             </div>
-        </Footer>
-        </div>
-    </>
+        </footer>
   )
 }
 
-export default FooterFin
+export default Footer
