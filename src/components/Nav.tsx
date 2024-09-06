0138deaaf8c6bc43a1ea4977190d1c67589db7b9
antohorta <antonia.horta.97@gmail.com>
2024-09-06 17:01:24 -0400

    Se agrega menú desplegable en Categorías

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -10,44 +10,44 @@ function Nav() {
         <ul className="menu">
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">Nosotros</Link></li>
-            <li>
+            <li className='categorias'>
               <Link to="/categorias">Categorías</Link>              
               <div className='caja-categoria-desplegable'>
                 <ul className='columna-categoria-1'>
-                  <li>Aventura</li>
-                  <li>Arte</li>
-                  <li>Biografías y autobiografías</li>
-                  <li>Ciencia ficción</li>
-                  <li>Ciencia y matemáticas</li>
-                  <li>Clásico</li>
-                  <li>Cocina</li>
+                  <li><Link to="/categorias">Aventura</Link></li>
+                  <li><Link to="/categorias">Arte</Link></li>
+                  <li><Link to="/categorias">Biografías y autobiografías</Link></li>
+                  <li><Link to="/categorias">Ciencia ficción</Link></li>
+                  <li><Link to="/categorias">Ciencia y matemáticas</Link></li>
+                  <li><Link to="/categorias">Clásico</Link></li>
+                  <li><Link to="/categorias">Cocina</Link></li>
                 </ul>
                 <ul className='columna-categoria-2'>
-                  <li>Computación</li>
-                  <li>Contemporáneo</li>
-                  <li>Derecho y política</li>
-                  <li>Distópia</li>
-                  <li>Economía y finanzas</li>
-                  <li>Educación</li>
-                  <li>Fantasía</li>
+                  <li><Link to="/categorias">Computación</Link></li>
+                  <li><Link to="/categorias">Contemporáneo</Link></li>
+                  <li><Link to="/categorias">Derecho y política</Link></li>
+                  <li><Link to="/categorias">Distópia</Link></li>
+                  <li><Link to="/categorias">Economía y finanzas</Link></li>
+                  <li><Link to="/categorias">Educación</Link></li>
+                  <li><Link to="/categorias">Fantasía</Link></li>
                 </ul>
-                <ul className='columna-categoria-2'>
-                  <li>Filosofía y religión</li>
-                  <li>Infantil</li>
-                  <li>Historia</li>
-                  <li>Juvenil</li>
-                  <li>Novela</li>
-                  <li>Novela histórica</li>
-                  <li>Paranormal</li>
+                <ul className='columna-categoria-3'>
+                  <li><Link to="/categorias">Filosofía y religión</Link></li>
+                  <li><Link to="/categorias">Infantil</Link></li>
+                  <li><Link to="/categorias">Historia</Link></li>
+                  <li><Link to="/categorias">Juvenil</Link></li>
+                  <li><Link to="/categorias">Novela</Link></li>
+                  <li><Link to="/categorias">Novela histórica</Link></li>
+                  <li><Link to="/categorias">Paranormal</Link></li>
                 </ul>
-                <ul className='columna-categoria-2'>
-                  <li>Poesía</li>
-                  <li>Romance</li>
-                  <li>Salud y deporte</li>
-                  <li>Técnicos y especializados</li>
-                  <li>Terror</li>
-                  <li>Thriller</li>
-                  <li>Viajes</li>
+                <ul className='columna-categoria-4'>
+                  <li><Link to="/categorias">Poesía</Link></li>
+                  <li><Link to="/categorias">Romance</Link></li>
+                  <li><Link to="/categorias">Salud y deporte</Link></li>
+                  <li><Link to="/categorias">Técnicos y especializados</Link></li>
+                  <li><Link to="/categorias">Terror</Link></li>
+                  <li><Link to="/categorias">Thriller</Link></li>
+                  <li><Link to="/categorias">Viajes</Link></li>
                 </ul>
               </div>
 
