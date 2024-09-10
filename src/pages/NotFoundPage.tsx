fb3cc826eaa34901b917b69f2d39878c2d8d0f91
antohorta <antonia.horta.97@gmail.com>
2024-09-10 10:02:04 -0300

    se agrega componente NotFound

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,8 +1,29 @@
 import { Navigate } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import { MainLayout } from "../layouts/MainLayout";
+import { NotFound } from "../components/NotFound";
 
 export const NotFoundPage = () => {
+    const [redirect, setRedirect] = useState(false);
+
+    useEffect(() => {
+        // temporizador de 3 segundos
+        const timer = setTimeout(() => {
+            setRedirect(true);
+        }, 3000);
+
+        // limpia temporizador si componente se desmonta antes de que se ejecute
+        return () => clearTimeout(timer);
+    }, []);
+
+    // después de 3 segundos se vuelve al home
+    if (redirect) {
+        return <Navigate to="/" />;
+    }
 
     return (
-        <Navigate to="/"/>
-    )
-}
+        <MainLayout>
+            <NotFound/>
+        </MainLayout>
+    );
+};
