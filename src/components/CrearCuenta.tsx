a037dbb42e5477e2a1197e6716ee53766ab0d700
antohorta <antonia.horta.97@gmail.com>
2024-09-23 17:19:44 -0300

    se agregan inputs relacionados a dirección en form de crear cuenta

--- src/components/CrearCuenta.tsx
+++ src/components/CrearCuenta.tsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom'
 import '../styles/login.css'
 const CrearCuenta = () => {
     return (
@@ -6,22 +5,50 @@ const CrearCuenta = () => {
             <p>¡Se parte de nuestra familia de lectores!</p>
             <h3>Crea una cuenta</h3>
             <form className="form-crear-cuenta">
-                <label htmlFor="nombres">Nombres</label>
-                <input type="text" id='nombres' placeholder="Ej: Roberto Andrés" required/>
+                <div className='form-crear-cuenta-row'>
+                    <div className='crear-cuenta-column-1'>
+                        <label htmlFor="nombres">Nombres</label>
+                        <input type="text" id='nombres' placeholder="Ej: Roberto Andrés" required/>
 
-                <label htmlFor="apellido-paterno">Apellido Paterno</label>
-                <input type="text" id='apellido-paterno' placeholder="Ej: Gonzáles" required/>
+                        <label htmlFor="apellido-paterno">Apellido Paterno</label>
+                        <input type="text" id='apellido-paterno' placeholder="Ej: Gonzáles" required/>
 
-                <label htmlFor="apellido-materno">Apellido Materno</label>
-                <input type="text" id='apellido-materno' placeholder="Ej: Ramírez" required/> 
+                        <label htmlFor="apellido-materno">Apellido Materno</label>
+                        <input type="text" id='apellido-materno' placeholder="Ej: Ramírez" required/> 
 
-                <label htmlFor="correo">Correo electrónico</label>
-                <input type="email" id='correo' placeholder="Ej: tuemail@gmail.com" required/>
+                        <label htmlFor="correo">Correo electrónico</label>
+                        <input type="email" id='correo' placeholder="Ej: tuemail@gmail.com" required/>
 
-                <label htmlFor="constrasena">Contraseña</label>
-                <input type="password" id='constrasena' placeholder="Crea una contraseña segura" required/>  
+                        <label htmlFor="constrasena">Contraseña</label>
+                        <input type="password" id='constrasena' placeholder="Crea una contraseña segura" required/> 
 
-                <button type="submit">Crear Cuenta</button>
+                        <label htmlFor="calle">Calle</label>
+                        <input type="text" id='calle' placeholder="Ej: Av. Salvador" required/>                
+                    </div>
+
+                    <div className='crear-cuenta-column-2'>
+                        <label htmlFor="numero-calle">Número de calle</label>
+                        <input type="text" id='numero-calle' placeholder="Ej: 183" required/>
+
+                        <label htmlFor="numero-depto">Número departamento</label>
+                        <input type="text" id='numero-depto' placeholder="Ej: 101"/>
+
+                        <label htmlFor="comuna">Comuna</label>
+                        <input type="text" id='comuna' placeholder="Ej: Santiago" required/>
+
+                        <label htmlFor="ciudad">Ciudad</label>
+                        <input type="text" id='ciudad' placeholder="Ej: Santiago" required/>
+
+                        <label htmlFor="region">Región</label>
+                        <input type="text" id='region' placeholder="Ej: Región Metropolitana" required/>
+
+                        <label htmlFor="info-adicional">Información adicional</label>
+                        <input type="text" id='info-adicional' placeholder="Agregue información adicional"/>
+                    </div>
+                </div>
+
+                <button type="submit" className='boton-crear-cuenta'>Crear Cuenta</button>
+                
             </form>
         </div>
     )
