fd11f9aaaeb1709f9776891e2d170bfcc4c9a374
Alexandra Pavez <alealecami@gmail.com>
2024-10-13 18:25:53 -0300

    creación del store

--- src/main.tsx
+++ src/main.tsx
@@ -15,10 +15,13 @@ import { UserPage } from './pages/UserPage.tsx'
 import { AuthProvider } from './auth/AuthContext.tsx'
 import ProtectedRoute from './auth/ProtectedRoute.tsx'
 import './styles/global.css'
+import { Provider } from 'react-redux'
+import { store } from './states/store.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthProvider>
+    <Provider store={store}>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage title={'Páginas Selectas'} />} />
@@ -34,6 +37,7 @@ createRoot(document.getElementById('root')!).render(
           <Route path="*" element={<NotFoundPage title='Página No Encontrada' />} />
         </Routes>
       </BrowserRouter>
+    </Provider>
     </AuthProvider>
   </StrictMode>,
 );
