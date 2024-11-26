5de348f4e7f884570cb74b13994e9be05e5d287c
Daniela Gajardo <dgajardoiglesias@gmail.com>
2024-11-25 23:05:21 -0300

    se agrega carrito de compras con diseño de ux

--- src/components/shoppingcart/ShoppingCart.tsx
+++ src/components/shoppingcart/ShoppingCart.tsx
@@ -3,11 +3,12 @@ import { RootType } from "../../states/store";
 import { ShoppingCartEntrada } from "../../interfaces/ShoppingCartEntrada";
 import '../../styles/shopping_cart.css'
 import ButtonDeleteToCart from "./ButtonDeleteToCart";
-import {  useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import ButtonClearCart from "./ButtonClearCart";
 import { Link, useNavigate } from "react-router-dom";
 import QuantityButtons from "./QuantityButtons";
 import { useEffect } from "react";
+import { Button, Col, Container, Row, Image } from "react-bootstrap";
 
 
 function ShoppingCart() {
@@ -34,83 +35,105 @@ function ShoppingCart() {
     }, [shoppingCartProduct, navigate])
 
     return (
-        <div className='shoppingcart'>
-            <div className="shoppingcart-item-top-footer">
-                <h2>Carro de Compras</h2>
-                <div className="shoppingcart-button-options">
-                    <Link to={`/categorias`}>
-                        <p className='shoppingcart-to-product'>Seguir comprando</p>
-                    </Link>
-                    <div className="shoppingcart-clear">
-                        <ButtonClearCart></ButtonClearCart>
+
+        <>
+
+            <Container fluid>
+                <div className='shoppingcart'>
+                    <Row md="12" style={{ height: '4rem', width: '75.25rem', margin: '1.5rem' }}>
+                        <Col md="5">
+                            <h2 className='fw-bold'>Carrito de Compras</h2>
+                        </Col>
+                        <Col md="3"></Col>
+                        <Col md="2">
+                            <Link to={`/categorias`}>
+                                <Button style={{ backgroundColor: '#975C4C', color: '#FBFBFB', border: '#E1D5CA' }} >
+                                    Seguir comprando
+                                </Button>
+                            </Link>
+                        </Col>
+                        <Col md="2">
+                            <ButtonClearCart></ButtonClearCart>
+                        </Col>
+                    </Row>
+                </div>
+            </Container>
+            <div className='shoppingcart'>
+                <Container fluid>
+                    {shoppingCartProduct.length ? (
+                        <div className='shoppingcart-items'>
+                            <Row key='header' md="12" style={{ height: '4rem', width: '75.25rem', margin: '1.5rem', color: '#545454' }}>
+                                <Col md="6">
+                                    <p className='fw-bold'>Productos</p>
+                                </Col>
+                                <Col md="1">
+                                </Col>
+                                <Col md="2">
+                                    <p className='d-flex align-items-center justify-content-center fw-bold'>Cantidad</p>
+                                </Col>
+                                <Col md="1">
+                                </Col>
+                                <Col md="2">
+                                    <p className='d-flex align-items-center justify-content-center fw-bold'>Precio</p>
+                                </Col>
+                            </Row>
+                            {shoppingCartProduct.map((item) => (
+
+
+                                <Row key={item.isbn} md="12" style={{ height: '15rem', width: '75.25rem', margin: '1.5rem', color: '#545454' }}>
+                                    <Col md="2">
+                                        <Image src='https://placehold.co/800@3x.png' fluid></Image>
+                                    </Col>
+                                    <Col md="1">
+                                    </Col>
+                                    <Col md="3" style={{ height: '9.18rem', width: '15.94rem', margin: '1.5rem' }}>
+                                        <p>{item.nombre}</p>
+                                        <p >{item.autor}</p>
+                                        <p >${item.precio}</p>
+
+                                    </Col>
+                                    <Col md="1">
+                                    </Col>
+                                    <Col md="2" className="d-flex align-items-center justify-content-center" style={{ height: '3rem', width: '14.688rem' }}>
+                                        <div className="shoppingcart-item-detail-td-quantity-1">
+                                            <QuantityButtons isbn={item.isbn} />
+                                            <ButtonDeleteToCart libro={item}></ButtonDeleteToCart>
+                                        </div>
+                                    </Col>
+                                    <Col md="1">
+                                    </Col>
+                                    <Col md="2" className="d-flex align-items-center justify-content-center" style={{ height: '3.18rem', width: '8.75rem' }}>
+                                        <p className='fw-bold'>${calculateTotalProduct(item)}</p>
+                                    </Col>
+                                </Row>
+
+                            ))}
+                        </div>
+                    ) : (
+                        <div>No existen productos en el carrito de compras.</div>
+                    )}
+                    <div className="shoppingcart-item-top-footer">
+
+                        <Row md="12" style={{ height: '4rem', width: '75.25rem', margin: '1.5rem' }}>
+                            <Col md="5">
+                                <h2 className='fw-bold'>Total a Pagar: ${calculateTotal(shoppingCartProduct)}</h2>
+                            </Col>
+                            <Col md="5"></Col>
+
+                            <Col md="2">
+                                <Link to={`/shoppingcart-resume/`}>
+                                    <Button  style={{ backgroundColor: '#975C4C', color: '#FBFBFB', border: '#E1D5CA' }} >
+                                        Comprar Ahora
+                                    </Button>
+                                </Link>
+                            </Col>
+                        </Row>
+
+
                     </div>
-                </div>
+                </Container>
             </div>
-            {shoppingCartProduct.length ? (
-                <div className='shoppingcart-items'>
-                    <table className="shoppingcart-items-table">
-                        <thead>
-                            <tr className="shoppingcart-item-detail-tr">
-                                <th className="shoppingcart-item-detail-th" colSpan={2} scope="col">
-                                    Producto
-                                </th>
-                                <th className="shoppingcart-item-detail-th" colSpan={1} scope="col">
-                                    Cantidad
-                                </th>
-                                <th className="shoppingcart-item-detail-th" colSpan={1} scope="col">
-                                    Total
-                                </th>
-                            </tr>
-                        </thead>
-                        {shoppingCartProduct.map((item) => (
-
-                            <>
-
-                                <tbody className='shoppingcart-item-detail'>
-                                    <tr className="shoppingcart-item-detail-tr">
-                                        <td className="shoppingcart-item-detail-td-image">
-                                            <img src='https://placehold.co/800@3x.png' alt={item.nombre} />
-                                        </td>
-                                        <td className="shoppingcart-item-detail-td-data">
-                                            <div className="shoppingcart-item-detail-td-nombre">
-                                                <label htmlFor="nombre-libro">Nombre: </label>
-                                                <p>{item.nombre}</p>
-                                                <p className='shoppingcart-item-detail'>{item.autor}</p>
-                                            </div>
-                                            <div className="shoppingcart-item-detail-td-autor">
-                                                <label htmlFor="autor-libro">Autor: </label>
-                                                <p className='shoppingcart-item-detail'>{item.autor}</p>
-                                            </div>
-                                        </td>
-                                        <td className="shoppingcart-item-detail-td-quantity">
-                                            <div className="shoppingcart-item-detail-td-quantity-1">
-                                                <QuantityButtons isbn={item.isbn} />
-                                                <ButtonDeleteToCart libro={item}></ButtonDeleteToCart>
-                                            </div>
-                                        </td>
-
-                                        <td className="shoppingcart-item-detail-td-total">
-                                            <p className='shoppingcart-item-total'>{calculateTotalProduct(item)}</p>
-                                        </td>
-                                    </tr>
-                                </tbody>
-
-                            </>
-                        ))}
-                    </table>
-                </div>
-            ) : (
-                <div>No existen productos en el carrito de compras.</div>
-            )}
-
-            <div className="shoppingcart-item-top-footer">
-                <h2>Total: ${calculateTotal(shoppingCartProduct)}</h2>
-                <Link to={`/shoppingcart-resume/`}>
-                    <button className="button-shoppingcart-resume">Pagar pedido</button>
-                </Link>
-            </div>
-
-        </div>
+        </>
     );
 };
 
