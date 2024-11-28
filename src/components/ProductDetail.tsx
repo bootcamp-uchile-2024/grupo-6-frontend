50fce907336fc687ee691b2b0a3830853b801b6a
Alexandra Pavez <alealecami@gmail.com>
2024-11-28 01:07:05 -0300

    perfil usuario y administrador, pagina detalle

--- src/components/ProductDetail.tsx
+++ src/components/ProductDetail.tsx
@@ -2,19 +2,47 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { ILibro } from '../interfaces/ILibro';
 import '../styles/product_detail.css'
-import estrellaLlena from '../assets/images/estrella_llena.png'
-import estrellaVacia from '../assets/images/estrella_vacia.png'
 import ButtonAddToCart from './ButtonAddToCart';
 import { ShoppingCartEntrada } from '../interfaces/ShoppingCartEntrada';
 import QuantityButtons from './shoppingcart/QuantityButtons';
 import { configuracion } from '../config/appConfiguration.ts';
+import { Container, Row, Col, Card } from "react-bootstrap";
+import libroBeatles from '../assets/images/Libros/libro beatles.webp'
+import libroInvitadoDracula from '../assets/images/Libros/invitado-dracula.webp'
+import libroDibujoFacil from '../assets/images/Libros/libro dibujo facil.webp'
+import libroTreeHouses from '../assets/images/Libros/tree-houses.webp'
+import '../styles/categorias.css';
+
+const recomendaciones = [
+    {
+        title: "The Beatles on the road",
+        author: "Harry Benson",
+        image: libroBeatles,
+    },
+    {
+        title: "Tree houses",
+        author: "Philip Jodidio",
+        image: libroTreeHouses,
+    },
+    {
+        title: "Dibujo fácil",
+        author: "Chelsea Ward",
+        image: libroDibujoFacil,
+    },
+    {
+        title: "El invitado de Drácula",
+        author: "Dram Stoker",
+        image: libroInvitadoDracula,
+    }
+];
+
 
 const ProductDetail: React.FC = () => {
     const { isbn } = useParams<{ isbn: string }>();
     const [libro, setLibro] = useState<ILibro | null>(null);
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<string | null>(null);
-    
+
     const [producto, setProducto] = useState<ShoppingCartEntrada>({
         nombre: '',
         autor: [""],
@@ -54,84 +82,118 @@ const ProductDetail: React.FC = () => {
 
         fetchProduct();
     }, [isbn]);
-    
+
     if (loading) return <div>Cargando...</div>;
     if (error) return <div>{error}</div>;
 
     return (
-        <div className='container-detail'>
+        <Container className="product-detail-container">
             {libro ? (
                 <>
-                    <div className='product-detail'>
-                        <div className='img-libro-detail'>
-                            <img src='https://placehold.co/800@3x.png' alt={libro.nombre} />
-                        </div>
-                        <div className='info-libro-detail'>
-                            <h1>{libro.nombre}</h1>
-                            <h3 className='autor-detail'>{libro.autor.join(', ')}</h3>
-                            <p className='resumen-detail'>{libro.resumen}</p>
-                            <div className='caja-estrellas-detail'>
-                                <img src={estrellaLlena} alt="Estrella llena" />
-                                <img src={estrellaLlena} alt="Estrella llena" />
-                                <img src={estrellaLlena} alt="Estrella llena" />
-                                <img src={estrellaVacia} alt="Estrella vacía" />
-                                <img src={estrellaVacia} alt="Estrella vacía" />
-                            </div>
-                            <p className='precio-detail'>Precio: ${libro.precio}</p>
-                            
-                            <ButtonAddToCart libro={producto}></ButtonAddToCart>
-                            {isbn && <QuantityButtons isbn={isbn} />}
-                        </div>
-                    </div>
+                    <Row className="product-detail">
+                        {/* Imagen del libro */}
+                        <Col md={4} className="product-image">
+                            <Card>
+                                <Card.Img
+                                    variant="top"
+                                    src="https://placehold.co/446x546"
+                                    alt={libro.nombre}
+                                />
+                            </Card>
+                        </Col>
 
-                    <div className='libro-descripcion-detail'>
-                        <h2>Ficha técnica</h2>
-                        <table>
-                            <tbody>
-                                <tr>
-                                    <td>ISBN:</td>
-                                    <td>{libro.isbn}</td>
-                                </tr>
-                                <tr>
-                                    <td>Editorial:</td>
-                                    <td>{libro.editorial}</td>
-                                </tr>
-                                <tr>
-                                    <td>Encuadernación:</td>
-                                    <td>{libro.encuadernacion}</td>
-                                </tr>
-                                <tr>
-                                    <td>Género(s):</td>
-                                    <td>{libro.genero.join(', ')}</td>
-                                </tr>
-                                <tr>
-                                    <td>Idioma:</td>
-                                    <td>{libro.idioma}</td>
-                                </tr>
-                                <tr>
-                                    <td>Año de publicación:</td>
-                                    <td>{libro.agnoPublicacion}</td>
-                                </tr>
-                                <tr>
-                                    <td>Número de páginas:</td>
-                                    <td>{libro.numeroPaginas}</td>
-                                </tr>
-                                <tr>
-                                    <td>Dimensiones:</td>
-                                    <td>{libro.dimensiones}</td>
-                                </tr>
-                                <tr>
-                                    <td>Código de barra:</td>
-                                    <td>{libro.ean}</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
+                        {/* Información del libro */}
+                        <Col md={8} className="productDeteil-info">
+                            <h1 className='productDetail-title-info'>{libro.nombre}</h1>
+                            <h3 className="autor-info">{libro.autor.join(", ")}</h3>
+                            <p className="price">${libro.precio}</p>
+
+                            {/* Estrellas de calificación */}
+                            <div className="rating-stars">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="24" viewBox="0 0 18 24" fill="none">
+                                    <path d="M4.5 0C2.01473 0 0 2.01473 0 4.5V19.5C0 21.9853 2.01473 24 4.5 24H17C17.5523 24 18 23.5523 18 23V22C18 21.4477 17.5523 21 17 21H4C3.44772 21 3 20.5523 3 20V19C3 18.4477 3.44772 18 4 18H17C17.5523 18 18 17.5523 18 17V1C18 0.447716 17.5523 0 17 0H4.5Z" fill="#545454" />
+                                </svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="24" viewBox="0 0 18 24" fill="none">
+                                    <path d="M4.5 0C2.01473 0 0 2.01473 0 4.5V19.5C0 21.9853 2.01473 24 4.5 24H17C17.5523 24 18 23.5523 18 23V22C18 21.4477 17.5523 21 17 21H4C3.44772 21 3 20.5523 3 20V19C3 18.4477 3.44772 18 4 18H17C17.5523 18 18 17.5523 18 17V1C18 0.447716 17.5523 0 17 0H4.5Z" fill="#545454" />
+                                </svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="24" viewBox="0 0 18 24" fill="none">
+                                    <path d="M4.5 0C2.01473 0 0 2.01473 0 4.5V19.5C0 21.9853 2.01473 24 4.5 24H17C17.5523 24 18 23.5523 18 23V22C18 21.4477 17.5523 21 17 21H4C3.44772 21 3 20.5523 3 20V19C3 18.4477 3.44772 18 4 18H17C17.5523 18 18 17.5523 18 17V1C18 0.447716 17.5523 0 17 0H4.5Z" fill="#545454" />
+                                </svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="24" viewBox="0 0 18 24" fill="none">
+                                    <path d="M4.5 0C2.01473 0 0 2.01473 0 4.5V19.5C0 21.9853 2.01473 24 4.5 24H17C17.5523 24 18 23.5523 18 23V22C18 21.4477 17.5523 21 17 21H4C3.44772 21 3 20.5523 3 20V19C3 18.4477 3.44772 18 4 18H17C17.5523 18 18 17.5523 18 17V1C18 0.447716 17.5523 0 17 0H4.5Z" fill="#545454" />
+                                </svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="24" viewBox="0 0 18 24" fill="none">
+                                    <path d="M4.5 0C2.01473 0 0 2.01473 0 4.5V19.5C0 21.9853 2.01473 24 4.5 24H17C17.5523 24 18 23.5523 18 23V22C18 21.4477 17.5523 21 17 21H4C3.44772 21 3 20.5523 3 20V19C3 18.4477 3.44772 18 4 18H17C17.5523 18 18 17.5523 18 17V1C18 0.447716 17.5523 0 17 0H4.5Z" fill="#D4D4D4" />
+                                </svg>
+                            </div>
+
+                            {/* Botones de cantidad y compra */}
+                            <div className="d-flex align-items-center my-3">
+                                <div className='catalog-buttons-container'>
+                                    <div className="quantity-section">
+                                        {isbn && <QuantityButtons isbn={isbn} />}
+                                    </div>
+                                    <ButtonAddToCart libro={producto} />
+                                </div>
+                            </div>
+
+                            {/* Información adicional */}
+                            <ul className="productDetail-specs">
+                                <li>
+                                    Editorial: {libro.editorial}
+                                </li>
+                                <li>
+                                    Encuadernación: {libro.encuadernacion}
+                                </li>
+                                <li>
+                                    Idioma: {libro.idioma}
+                                </li>
+                                <li>
+                                    Páginas: {libro.numeroPaginas}
+                                </li>
+                                <li>
+                                    Dimensiones: {libro.dimensiones}
+                                </li>
+                            </ul>
+                        </Col>
+                    </Row>
+
+                    {/* Argumento */}
+                    <Row className="productDetail-argumento-container">
+                        <Col>
+                            <h3>Argumento</h3>
+                            <p>{libro.resumen}</p>
+                        </Col>
+                    </Row>
+
+                    {/* Recomendaciones */}
+                    <Row className="productDetail-container-recomendaciones">
+                        <Col className='productDetail-col-recomendaciones'>
+                            <h3 className='productDetail-title-recomendaciones'>Recomendaciones</h3>
+                            <Row className='productDetail-container-recomendaciones-card'>
+                                {recomendaciones.map((rec, index) => (
+                                    <Col md={3} key={index} className='productDetail-recomendaciones-card'>
+                                        <Card>
+                                            <Card.Img
+                                                variant="top"
+                                                src={rec.image}
+                                                alt={rec.title}
+                                            />
+                                            <Card.Body className='productDetail-recomendaciones-info'>
+                                                <Card.Title>{rec.title}</Card.Title>
+                                                <Card.Text>{rec.author}</Card.Text>
+                                            </Card.Body>
+                                        </Card>
+                                    </Col>
+                                ))}
+                            </Row>
+                        </Col>
+                    </Row>
                 </>
             ) : (
                 <div>No se encontró el producto</div>
             )}
-        </div>
+        </Container>
     );
 };
 
