199d05abc116719efde0ad28e0719e79286e1dfe
antohorta <antonia.horta.97@gmail.com>
2024-12-05 18:02:22 -0300

    avance de filtros de generos

--- src/components/Filtros.tsx
+++ src/components/Filtros.tsx
@@ -1,86 +1,97 @@
 import { useState, useEffect } from 'react';
 import '../styles/filtros.css';
-/* import CategoryElement from './CategoryElement.tsx';
-import { useEffect, useState } from 'react'; */
 
-// Interface para la categoría
-/* interface Category {
-  idGenero: string;
-  nombreGenero: string;
-  subGeneros: string[];
-} */
+interface FiltrosProps {
+  actualizarGeneros: (generos: string[]) => void;
+}
 
-function Filtros() {
+function Filtros({ actualizarGeneros }: FiltrosProps) {
 
   const [generos, setGeneros] = useState<string[]>([]);
+  const [generosFiltrados, setGenerosFiltrados] = useState<Record<string, boolean>>({});
 
   useEffect(() => {
     fetch('http://18.222.107.138:3000/products/genres')
-      .then((response) => response.json())
-      .then((data) => setGeneros(data))
-      .catch((error) => console.error("Hubo un error al obtener los géneros:", error));
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Error en la respuesta del servidor');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log('Géneros recibidos:', data); // para ver géneros que llegan
+        setGeneros(data);
+      })
+      .catch((error) => console.error('Hubo un error:', error));
   }, []);
-  /* const [category, setCategory] = useState<Category[]>([]);
-  const [categoryExist, setCategoryExist] = useState<boolean>(false);
+  
 
-  useEffect(() => {
-    async function getCategories() {
-      try {
+  const handleOnCheckbox = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const nuevoEstado = {
+      ...generosFiltrados,
+      [e.target.value]: e.target.checked,
+    };
+    console.log('Estado actualizado:', nuevoEstado); // géneros que están marcados
+    setGenerosFiltrados(nuevoEstado);
 
-        // Simulando la respuesta del backend
-        const productJson = [{
-          "idGenero": "1",
-          "nombreGenero": "Aventura",
-          "subGeneros": []
-        }];
+    const generosSeleccionados = Object.keys(nuevoEstado).filter(
+      (genero) => nuevoEstado[genero]
+    );
+    console.log('Géneros seleccionados:', generosSeleccionados);
+    actualizarGeneros(generosSeleccionados); // Notifica a Categorias los géneros seleccionados
+  };
 
-        setCategory(productJson);
-        setCategoryExist(true);
+    return (
+      <>
+        <div className="filtros-categoria">
+          <h2 className="titulo-filtro">Filtros</h2>
+          <h2 className="titulo-filtro">Categorías</h2>
 
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      } catch (error) {
-        console.log('Error al obtener las categorias');
-        setCategoryExist(false);
-      }
-    }
-
-    getCategories();
-  }, []);
- */
-  return (
-    <>
-      <div className="filtros-categoria">
-        <h2 className="titulo-filtro">Filtros</h2>
-        <h2 className="titulo-filtro">Categorías</h2>
-
-        {/* filtros de generos*/}
-        <div className="checkbox-container">
-          {generos.map((genero) => (
-            <div key={genero}>
-              <input id={genero} type="checkbox" />
-              <label htmlFor={genero}>{genero}</label>
-            </div>
-          ))}
+          {/* filtros de generos*/}
+          <div className="checkbox-container">
+            {generos.map((genero) => (
+              <div key={genero}>
+                <input
+                  id={genero}
+                  type="checkbox"
+                  onChange={handleOnCheckbox}
+                  value={genero} />
+                <label htmlFor={genero}>{genero}</label>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
 
-      {/* filtros de precio*/}
-      <div className='price-filter-container'>
-        <h2 className="titulo-filtro">Precios entre</h2>
-        <div className='price-input-container'>
-          <input id="minimo" name="minimo" type='number' pattern="^[0-9]+([,.][0-9]+)?$" placeholder="$mínimo" />
-          <p className="titulo-filtro">y</p>
-          <input id="maximo" name="maximo" type="number" pattern="^[0-9]+([,.][0-9]+)?$" placeholder="$máximo" />
+        {/* filtros de precio*/}
+        <div className='price-filter-container'>
+          <h2 className="titulo-filtro">Precios entre</h2>
+          <div className='price-input-container'>
+            <input
+              id="minimo"
+              name="minimo"
+              type='number'
+              pattern="^[0-9]+([,.][0-9]+)?$"
+              placeholder="$mínimo" />
+            <p className="titulo-filtro">y</p>
+            <input
+              id="maximo"
+              name="maximo"
+              type="number"
+              pattern="^[0-9]+([,.][0-9]+)?$"
+              placeholder="$máximo" />
+          </div>
         </div>
-      </div>
 
-      <h2 className="titulo-filtro">Editorial</h2>
-      
-      {/* filtros de editorial*/}
-      <button className='filter-button'>Aplicar filtros</button>
+        {/* filtros de editorial*/}
+        <h2 className="titulo-filtro">Editorial</h2>
 
-    </>
-  );
-};
+        <button
+          type="button"
+          className='filter-button'
+        >Aplicar filtros</button>
 
-export default Filtros;
+      </>
+    );
+  };
+
+  export default Filtros;
