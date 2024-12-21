da6b61c6801b5e2ff54e6c55aafbde6e4690e1ee
Daniela Gajardo <dgajardoiglesias@gmail.com>
2024-12-21 19:51:29 -0300

    update resumen

--- src/components/shoppingcart/ResumenShoppingCart.tsx
+++ src/components/shoppingcart/ResumenShoppingCart.tsx
@@ -11,10 +11,14 @@ import Image from "react-bootstrap/esm/Image";
 import Dropdown from "react-bootstrap/esm/Dropdown";
 import { useState } from "react";
 import { configuracion } from "../../config/appConfiguration";
+import DropdownButton from "react-bootstrap/esm/DropdownButton";
+import Form from "react-bootstrap/esm/Form";
 
 function ResumenShoppingCart() {
     const shoppingCartProduct = useSelector((state: RootType) => state.productReducer.cart.items);
     const [selectedAddress, setSelectedAddress] = useState("Los Olmos 666, Macul, RM");
+    const [despacho] = useState(5000);
+
     const url = configuracion.urlJsonServerBackendCover.toString();
 
     const addresses = [
@@ -38,6 +42,15 @@ function ResumenShoppingCart() {
         return initialTotal;
     }
 
+    // Calcula el total del carrito de compras + despacho
+    const calculateTotalConDespacho = (items: ShoppingCartEntrada[]) => {
+        let initialTotal = 0;
+        items.forEach(item => {
+            initialTotal = initialTotal + item.precio * item.cantidad;
+        });
+        return initialTotal + despacho;
+    }
+
     //Calcula el total de cada producto basado en la cantidad
     const calculateTotalProduct = (item: ShoppingCartEntrada) => {
         return item.precio * item.cantidad;
@@ -45,123 +58,108 @@ function ResumenShoppingCart() {
 
     return (
         <div className='resumen-shopping-cart'>
-
-            <Container fluid>
-                <div className="item-top-resumen">
-                    <Row md="12" style={{ height: '4rem', width: '75.25rem', margin: '1.5rem' }}>
-                        <Col md="5">
+            <Container>
+                <div className='shoppingcart'>
+                    <Row>
+                        <Col lg={8}>
                             <h2 className='fw-bold'>Resumen de compra</h2>
                         </Col>
-                        <Col md="3"></Col>
-                        <Col md="2">
-                            <div className="back-to-carrito">
-                                <Link to={`/carrito`}>
-                                    <Button style={{ backgroundColor: '#E1D5CA', color: '#975C4C', border: '#E1D5CA' }} >
-                                        Volver a carrito
-                                    </Button>
-                                </Link>
-                            </div>
+                        <Col className="clear-cart-button" lg={2}>
+                            <Link to={`/carrito`}>
+                                <Button style={{ backgroundColor: '#E1D5CA', color: '#975C4C', border: '#E1D5CA' }} >
+                                    Volver a carrito
+                                </Button>
+                            </Link>
                         </Col>
-                        <Col md="2">
-
-                            <Link to={`/shoppingcart-payment`}>
-                                <Button className="button-shoppingcart-resume" style={{ backgroundColor: '#975C4C', color: '#FBFBFB', border: '#E1D5CA' }}>
+                        <Col className="seguir-comprando-button" lg={2}>
+                            <Link to={`/categorias`}>
+                                <Button variant='none' className='md-2'>
                                     Pagar el pedido
                                 </Button>
                             </Link>
                         </Col>
                     </Row>
                 </div>
+
                 <div>
-                    <Row md="12" style={{ height: '4rem', width: '75.25rem', margin: '1.5rem' }}>
+                    <Row md="12" style={{ height: '4rem', width: '75.25rem' }}>
                         <Col md="5">
-                            <h3 className='fw-bold'>Total a Pagar: ${calculateTotal(shoppingCartProduct)}</h3>
+                            <h3 className='fw-bold'>Total a Pagar: ${calculateTotalConDespacho(shoppingCartProduct).toLocaleString()}</h3>
                         </Col>
                         <Col md="7">
                         </Col>
                     </Row>
                 </div>
 
+                <div>
+                    <Row md="12" style={{ height: '4rem', width: '75.25rem' }}>
+                        <Col md="6">
+                        </Col>
+                        <Col md="3">
+                            <h5 className='fw-bold'>Total carrito: ${calculateTotal(shoppingCartProduct).toLocaleString()}</h5>
+                            <h5 className='fw-bold'>Valor del despacho: ${despacho.toLocaleString()}</h5>
+                        </Col>
+                        <Col md="3">
+                        </Col>
+                    </Row>
+                </div>
+
                 {shoppingCartProduct.length ? (
                     <div className='shoppingcart-items'>
-                        <div className='shoppingcart-items'>
-                            <Row key='header' md="12" style={{ height: '4rem', width: '75.25rem', margin: '1.5rem', color: '#545454' }}>
-                                <Col md="6">
-                                    <p className='fw-bold'>Productos</p>
-                                </Col>
-                                <Col md="1">
-                                </Col>
+                        {shoppingCartProduct.map((item) => (
+                            <Row key={item.isbn} className="mb-3">
+                                <Col md="3"></Col>
                                 <Col md="2">
-                                    <p className='d-flex align-items-center justify-content-center fw-bold'>Cantidad</p>
+                                    <Image src={`${url}${item.caratula}`} alt={`imagen del libro ${item.nombre}`} style={{ width: '160px', height: '240px' }} fluid></Image>
                                 </Col>
-                                <Col md="1">
+
+                                <Col className="d-flex align-items-center justify-content-center cart-product-info" md="2">
+                                    <p>{item.nombre}</p>
                                 </Col>
-                                <Col md="2">
-                                    <p className='d-flex align-items-center justify-content-center fw-bold'>Precio</p>
+
+                                <Col className="d-flex align-items-center justify-content-center cart-product-info" md="2">
+                                    <p>${calculateTotalProduct(item).toLocaleString()}</p>
                                 </Col>
+                                <Col md="3"></Col>
                             </Row>
-                            {shoppingCartProduct.map((item) => (
-                                <Row key={item.isbn} md="12" style={{ height: '15rem', width: '75.25rem', margin: '1.5rem', color: '#545454' }}>
-                                    <Col md="2">
-                                    <Image src={`${url}${item.caratula}`} alt={`imagen del libro ${item.nombre}`} style={{width: '160px', height: '240px'}} fluid></Image>
-                                    </Col>
-                                    <Col md="1">
-                                    </Col>
-                                    <Col md="3" style={{ height: '9.18rem', width: '15.94rem', margin: '1.5rem' }}>
-                                        <p>{item.nombre}</p>
-                                        <p >{item.autor}</p>
-                                        <p >${item.precio}</p>
-
-                                    </Col>
-                                    <Col md="1">
-                                    </Col>
-                                    <Col md="2" className="d-flex align-items-center justify-content-center" style={{ height: '3rem', width: '14.688rem' }}>
-                                        <div className="shoppingcart-item-detail-td-quantity-1">
-                                            <p>{item.cantidad}</p>
-                                        </div>
-                                    </Col>
-                                    <Col md="1">
-                                    </Col>
-                                    <Col md="2" className="d-flex align-items-center justify-content-center" style={{ height: '3.18rem', width: '8.75rem' }}>
-                                        <p className='fw-bold'>${calculateTotalProduct(item)}</p>
-                                    </Col>
-                                </Row>
-
-
-                            ))}
-                        </div>
+                        ))}
                     </div>
 
                 ) : (
                     <div>No existen productos en el carrito de compras.</div>
                 )}
 
-                <div className="'shoppingcart-items">
-                        <Row md="12" style={{ height: '4rem', width: '75.25rem', margin: '1.5rem' }}>
-                            <Col md="6">
-                                <h4 className="d-flex  fw-bold">Despacho a domicilio</h4>
-                            </Col>
-                            <Col md="4">
-                                <Dropdown onSelect={(e) => handleSelectAddress(e)}   style={{ color:  '#FBFBFB', borderColor:  '#975C4C'}}>
-                                    <Dropdown.Toggle variant="outline-primary" id="dropdown-direcciones">
-                                        {selectedAddress || "Seleccionar dirección"}
-                                    </Dropdown.Toggle>
-                                    <Dropdown.Menu>
-                                        {addresses.map((address, index) => (
-                                            <Dropdown.Item key={index} eventKey={address}>
-                                                {address}
-                                            </Dropdown.Item>
-                                        ))}
-                                    </Dropdown.Menu>
-                                </Dropdown>
-                            </Col>
-                            <Col md="1" >
-                            </Col>
-                            <Col md="1" className="d-flex align-items-center justify-content-center" style={{ height: '3.18rem', width: '1rem' }}>
-                                <p className="fw-bold">$5000</p>
-                            </Col>
-                        </Row>
-                    </div>
+                <div className="d-flex align-items-center p-3 border rounded">
+                    {/* Seleccionar dirección */}
+                    <Row md="12" style={{ height: '4rem', width: '75.25rem' }}>
+                    <Form.Group className="flex-grow-1 me-3 d-flex align-items-center">
+                        <Col md="2"></Col>
+                        <Col md="4">
+                            <Form.Label>Seleccionar tu dirección</Form.Label>
+                        </Col>
+                        <Col md="4">
+                        <DropdownButton
+                            id="dropdown-address-selector"
+                            title={selectedAddress || 'Seleccionar dirección'}
+                            onSelect={handleSelectAddress}
+                            variant="outline-secondary"
+
+                        >
+                            {addresses.length > 0 ? (
+                                addresses.map((address, index) => (
+                                    <Dropdown.Item key={index} eventKey={address}>
+                                        {address}
+                                    </Dropdown.Item>
+                                ))
+                            ) : (
+                                <Dropdown.Item disabled>No hay direcciones disponibles</Dropdown.Item>
+                            )}
+                        </DropdownButton>
+                        </Col>
+                        <Col md="2"></Col>
+                    </Form.Group>
+                    </Row>
+                </div>
             </Container>
         </div>
     );
